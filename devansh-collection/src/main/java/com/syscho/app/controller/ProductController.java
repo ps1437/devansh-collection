5ac600e60ff6a195ebdce4b7f732743f855fde2f
ps1437 <praveen369soni@gmail.com>
2019-10-28 23:29:00 +0530

    'added'

--- devansh-collection/src/main/java/com/syscho/app/controller/ProductController.java
+++ devansh-collection/src/main/java/com/syscho/app/controller/ProductController.java
@@ -1,13 +1,47 @@
 package com.syscho.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.syscho.app.service.ProductService;
+import com.syscho.model.ProductDO;
 
 @Controller
+@RequestMapping("/devansh/product")
 public class ProductController {
 
 	@Autowired
 	ProductService service;
+
+	@PostMapping("/add")
+	public ProductDO addProduct(ProductDO productDO) {
+		return service.addProduct(productDO);
+	}
+
+	@PostMapping("/delete")
+	public ProductDO delProduct(ProductDO productDO) {
+		return service.delProduct(productDO);
+	}
+
+	@GetMapping("/view/by/name/{prodNumber}")
+	public List<ProductDO> getProductsByNumber(@PathVariable("prodNumber") String prodNumber) {
+		return service.getProductsByNumber(prodNumber);
+	}
+
+	@GetMapping("/view/by/number/{prodName}")
+	public List<ProductDO> getProducts(@PathVariable("prodName") String prodName) {
+		return service.getProductsByName(prodName);
+	}
+
+	@GetMapping("/view")
+	public List<ProductDO> getProducts() {
+		return service.getProducts();
+	}
+
 }
--- devansh-collection/src/main/java/com/syscho/app/service/ProductService.java
+++ devansh-collection/src/main/java/com/syscho/app/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.syscho.app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.syscho.app.ProductRepository;
+import com.syscho.model.ProductDO;
 
 @Service
 public class ProductService {
@@ -11,4 +14,30 @@ public class ProductService {
 	@Autowired
 	ProductRepository repository;
 
+	public List<ProductDO> getProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<ProductDO> getProductsByName(String prodName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<ProductDO> getProductsByNumber(String prodNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ProductDO delProduct(ProductDO productDO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ProductDO addProduct(ProductDO productDO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
